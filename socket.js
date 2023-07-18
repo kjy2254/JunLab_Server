4cf03db55a623b3199739e41da63b8d20be7a91d
kjy2254 <wkrwjafl15@gmail.com>
2023-07-18 18:10:28 +0900

    ID 등록과정 수정중

--- socket.js
+++ socket.js
@@ -8,9 +8,6 @@ var sockets = {};
 var commandSocket = undefined;
 var last_command = {};
 
-//hello server?
-const hello = Buffer.from([0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x3f]);
-
 function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
 }
@@ -25,33 +22,28 @@ commandServer.listen(4323, () => {
 
 commandServer.on('connection', (socket) => {
     commandSocket = socket;
-    socket.write("Connected to Server\r\n" +
+    socket.write("Welcome to Command Server\r\n" +
         "--------------------------------------------------------------------\r\n" +
         "Type this Command: \r\n " +
         "- Devices: Check registered devices \r\n " +
         "- ID_?/[command]: Send command to ID_?\r\n" +
         "--------------------------------------------------------------------\r\n");
-
-
     socket.on('data', (data) => {
-        if(data.toString().trim().toLowerCase() === "devices"){
+        if (data.toString().trim().toLowerCase() === "devices") {
             socket.write("\r\nRegistered devices: [" + Object.keys(sockets).toString() + "]\r\n");
-        }
-        else if(data.toString().trim().includes("/")){
+        } else if (data.toString().trim().includes("/")) {
             let commands = data.toString().trim().split('/');
             // console.log("ID:", commands[0]);
             // console.log("Command:", commands[1]);
 
-            if(Object.keys(sockets).includes(commands[0])){
+            if (Object.keys(sockets).includes(commands[0])) {
                 sockets[commands[0]].write(commands[1]);
-                last_command = {...last_command, [commands[0]]:commands[1]}
-                console.log(last_command);
-            }
-            else{
+                last_command = {...last_command, [commands[0]]: commands[1]}
+
+            } else {
                 socket.write("\r\n" + commands[0] + "is not registered\r\n");
             }
-        }
-        else{
+        } else {
             socket.write("" +
                 "--------------------------------------------------------------------\r\n" +
                 "Type this Command: \r\n " +
@@ -60,7 +52,6 @@ commandServer.on('connection', (socket) => {
                 "--------------------------------------------------------------------\r\n");
         }
     })
-
     socket.on('close', () => {
         console.log('command server disconnected');
     })
@@ -70,45 +61,60 @@ socketServer.on('connection', (socket) => {
     socket.on('data', (rawData) => {
         console.log(rawData.toString());
         let sensorData = rawData.toString().split(',');
-        // 디바이스 켜짐 체크
-        if(rawData.toString().trim() === "Hello server?"){
-            // 센서 데이터 1회 수신
-            socket.write("sensorTA,1,1000");
+        // // 디바이스 켜짐 체크
+        // if(rawData.toString().trim() === "Hello server?"){
+        //     // 센서 데이터 1회 수신
+        //     socket.write("sensorTA,1,1000");
+        // }
+        //
+        if (sensorData.length > 1 && sensorData[1].toString().includes("AP")) {
+            sockets = {...sockets, ["ID_" + sensorData[0]]: socket};
+            // socket.write("\r\nsensorTA,0,1000\r\n");
+            if (Object.keys(last_command).includes("ID_" + sensorData[0])) {
+                console.log("last command: ", last_command["ID_" + sensorData[0]]);
+                socket.write(last_command["ID_" + sensorData[0]]);
+            }
+            if (commandSocket != undefined) { // !! command 여러개일 경우 수정 필요
+                commandSocket.write("\r\n" + "ID_" + sensorData[0] + " is connected\r\n");
+                commandSocket.write("\r\nRegistered devices: [" + Object.keys(sockets).toString() + "]\r\n");
+                commandSocket.write("\r\n" + "run last command: " + last_command["ID_" + sensorData[0]] + "\r\n");
+            }
+            // socket.write("\r\nDevice"+sensorData[0]+" is registered\r\n");
         }
-        if(sensorData.length === 21){
-            if(Object.keys(sockets).includes("ID_"+sensorData[0])){ // 소켓이 등록되어 있는 경우
-                if (save(rawData)) {
-                    socket.write("\r\nData save success" + "\r\n");
-                } else {
-                    socket.write("\r\nSomething went wrong" + "\r\n");
-                }
+        if (sensorData.length === 21) {
+            if (save(rawData)) {
+                socket.write("\r\nData save success" + "\r\n");
+            } else {
+                socket.write("\r\nSomething went wrong" + "\r\n");
             }
-            else{
-                if(!isNaN(sensorData[0])){ // 첫 번째 인자가 숫자일 경우
-                    sockets = {...sockets, ["ID_"+sensorData[0]]:socket};
-                    socket.write("\r\nsensorTA,0,1000\r\n");
-                    socket.write("\r\nDevice"+sensorData[0]+" is registered\r\n");
-                    if(commandSocket != undefined){
-                        commandSocket.write("\r\n" + "ID_"+sensorData[0] + " is connected\r\n");
-                        commandSocket.write("\r\nRegistered devices: [" + Object.keys(sockets).toString() + "]\r\n");
-                    }
-                }
-            }
+            // if(Object.keys(sockets).includes("ID_"+sensorData[0])){ // 소켓이 등록되어 있는 경우
+            // }
+            // else{
+            //     if(!isNaN(sensorData[0])){ // 첫 번째 인자가 숫자일 경우
+            //         // sockets = {...sockets, ["ID_"+sensorData[0]]:socket};
+            //         // socket.write("\r\nsensorTA,0,1000\r\n");
+            //         // socket.write("\r\nDevice"+sensorData[0]+" is registered\r\n");
+            //         // if(commandSocket != undefined){
+            //         //     commandSocket.write("\r\n" + "ID_"+sensorData[0] + " is connected\r\n");
+            //         //     commandSocket.write("\r\nRegistered devices: [" + Object.keys(sockets).toString() + "]\r\n");
+            //         // }
+            //     }
+            // }
         }
     })
 
     socket.on('close', () => {
         // console.log(getKeyByValue(sockets, socket) + " is disconnected")
         console.log(commandSocket);
-        if(commandSocket != undefined){
-            commandSocket.write("\r\n"+getKeyByValue(sockets, socket) + " is disconnected\r\n");
+        if (commandSocket != undefined) {
+            commandSocket.write("\r\n" + getKeyByValue(sockets, socket) + " is disconnected\r\n");
         }
         delete sockets[getKeyByValue(sockets, socket)];
     })
 
     socket.on('error', function (exc) {
         console.log("Update Connection");
-        if(Object.keys(sockets).includes(last_command[0])) sockets[last_command[0]].write(last_command[1]);
+        if (Object.keys(sockets).includes(last_command[0])) sockets[last_command[0]].write(last_command[1]);
     });
 })
 
