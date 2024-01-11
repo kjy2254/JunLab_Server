ae1f6640c325b4d86dbc7b59c32ec4589922cf5f
min <dltkd2786@gmail.com>
2024-01-11 16:54:36 +0900

    account page 스크롤, input 추가

--- frontend/src/component/Account.js
+++ frontend/src/component/Account.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Header from "./Header";
+import edit from "../image/edit.svg";
 
 function Account(props) {
   const [filter, setFilter] = useState("");
@@ -37,32 +38,88 @@ function AccountComponent() {
     <div className="center">
       <div className="center bg-layer1 ac-bgcolor1">
         <div className="bg-layer2 ac-bgcolor2">
-          <p className="left-align-text">계정 정보</p>
-          <div className="scroll">
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>테스트</p>
-            <p>케케케</p>
-            <p>케케케</p>
-            <p>케케케</p>
-            <p>케케케</p>
-            <p>케케케</p>
-            <p>케케케</p>
-            <p>케케케</p>
+          <p className="left-align-text">
+            계정 정보
+            <button className="btn-container" onClick={() => {}}>
+              <img src={edit} width={20} className="img-form" />
+            </button>
+          </p>
+          <div className="Account-scroll">
+            <p className="margin-left-1 display-flex">
+              권한
+              <p className="account-title">미승인</p>
+            </p>
+            <p className="margin-left-1">
+              공장
+              <input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              관리자
+              <input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              연락처<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이메일<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              생년월일<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              추가<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              추가<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              추가<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              추가<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              추가<input className="input-form"></input>
+            </p>
           </div>
         </div>
         <div className="bg-layer2 ac-bgcolor2">
           <p className="left-align-text">직원 정보</p>
+          <div className="Account-scroll">
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+            <p className="margin-left-1">
+              이름<input className="input-form"></input>
+            </p>
+          </div>
         </div>
       </div>
     </div>
